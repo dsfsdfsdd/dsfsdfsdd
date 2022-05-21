38496edb2ff615d9cacd3ae6a140361c978df1cf
dsfsdfsdd <105963406+dsfsdfsdd@users.noreply.github.com>
2022-05-21 09:17:16 +0300

    Create Calculator.py

---
+++ Calculators/Calculator.py
@@ -0,0 +1,15 @@
+# Calculator
+
+while True:
+  a = input("Ведите первое число: ")
+  b = input("Ведите второе число: ")
+  с = input("Ведите +, -, *, /: ")
+  if c == "+":
+    print("a+b")
+  elif c == "-":
+    print("a-b")
+  elif c == "*":
+    print("a*b")
+  elif c == "+":
+    print("a/b")
+  
